3cae55884f5f4ebffad4200d2331cf1447707e2d
Christina Hall <cmcclai26@gmail.com>
2023-03-28 21:18:13 -0400

    post new category route business handled insomnia confirmed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,8 +40,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const createNewCategory = await Category.create(req.body);
+    res.status(200).json(createNewCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
